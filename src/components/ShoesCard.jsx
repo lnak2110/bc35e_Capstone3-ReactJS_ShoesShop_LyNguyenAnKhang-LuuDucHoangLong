35a025e91f4c98e38a20cf0fe2a1957b21f526dd
akhang <lnak211099@gmail.com>
2023-01-06 10:47:52 +0700

    Responsive fixed

--- src/components/ShoesCard.jsx
+++ src/components/ShoesCard.jsx
@@ -5,25 +5,27 @@ const ShoesCard = (props) => {
   const { prod } = props;
 
   return (
-      <div className="card mb-3">
-        <img src={prod?.image} alt="" />
-        <img src="./img/unlike.png" alt="" className="like" />
-        <div className="card-body">
-          <h3>
-            {prod?.name.length > 25
-              ? prod.name.substring(0, 20) + "..."
-              : prod.name}
-          </h3>
-          <p className="mb-5">{prod?.shortDescription}</p>
-          <NavLink
-            to={`/detail/${prod?.id}`}
-            className="btn btn-success to_detail"
-          >
-            View Detail
-          </NavLink>
-          <span className="btn btn-light price">$ {prod?.price}</span>
-        </div>
+    <div className="card mb-3">
+      <img src={prod?.image} alt="" />
+      <img src="./img/unlike.png" alt="" className="like" />
+      <div className="card-body">
+        <h3 className="product-name">
+          {prod?.name.length > 25
+            ? prod.name.substring(0, 20) + '...'
+            : prod.name}
+        </h3>
+        <p className="mb-5 product-short-description">
+          {prod?.shortDescription}
+        </p>
+        <NavLink
+          to={`/detail/${prod?.id}`}
+          className="btn btn-success to_detail"
+        >
+          View Detail
+        </NavLink>
+        <span className="btn btn-light price">$ {prod?.price}</span>
       </div>
+    </div>
   );
 };
 
